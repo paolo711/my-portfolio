044afdbc0b9eebddbf2d90fa2402ca1ba1b0fa09
paolojohnlatorilla711 <paolojohnlatorilla711@gmail.com>
2025-10-22 10:45:47 +0000

    Prepare for Render deployment

---
+++ server.js
@@ -0,0 +1,65 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import nodemailer from 'nodemailer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, 'dist')));
+
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_ADDRESS,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Ready to Send');
+  }
+});
+
+app.post('/contact', (req, res) => {
+  const { firstName, lastName, email, message, phone } = req.body;
+  const name = `${firstName} ${lastName}`;
+  const mail = {
+    from: name,
+    to: process.env.EMAIL_ADDRESS,
+    subject: 'Contact Form Submission - Portfolio',
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: 'Message Sent' });
+    }
+  });
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
